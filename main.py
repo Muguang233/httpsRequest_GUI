a4d0e6332e7ab4fb9262ed3a244452fa4f3ebb1c
Zellane <muguang233@gmail.com>
2025-09-11 17:43:38 +1000

    转换ui文件至py类封装并修改请求为线程任务

--- main.py
+++ main.py
@@ -7,8 +7,10 @@ from PySide6.QtWidgets import (
   QComboBox,
   QCheckBox,
   QDialog,
+  QWidget,
   QVBoxLayout,
-  QListWidget
+  QListWidget,
+  QMainWindow
 )
 from helper import (
   valid_prefix,
@@ -18,8 +20,9 @@ from helper import (
   get_saved_url,
   UrlSelectDialog
 )
+from ui import Ui_main
 from PySide6.QtUiTools import QUiLoader
-import sys, os
+import sys, os, threading
 def resource_path(relative_path):
     try:
         base_path = sys._MEIPASS
@@ -27,52 +30,57 @@ def resource_path(relative_path):
         base_path = os.path.abspath(".")
 
     return os.path.join(base_path, relative_path)
-
-class Mainwindow():
+class _MainWindow(QMainWindow):
   def __init__(self):
     super().__init__()
-    path = resource_path('ui/req.ui')
-    self.window = QUiLoader().load(path)
-    self.request_body = get_body(self.window.findChild(QTextEdit, "textEdit"))
-    self.logger = self.window.findChild(QTextBrowser, "textBrowser")
+    mainWidget = QWidget()
+    self.ui = Ui_main()
+    self.ui.setupUi(mainWidget)
+    self.setCentralWidget(mainWidget)
+    self.request_body = get_body(self.ui.textEdit)
     self.listener()
+  def clear_logger(self):
+     self.ui.textBrowser.clear()
+  
   def choose_url(self):
     saved_items = get_saved_url("url")
-    sub_window = UrlSelectDialog(saved_items, "./save/url.txt", self.window)
+    sub_window = UrlSelectDialog(saved_items, "./save/url.txt", self)
     if sub_window.exec() == QDialog.Accepted:
       url = sub_window.get_selected_url()
       if url:
-        self.window.findChild(QLineEdit, "lineEdit").setText(url)
+        self.ui.lineEdit.setText(url)
   def choose_endpoint(self):
     saved_items = get_saved_url("endpoint")
-    sub_window = UrlSelectDialog(saved_items, "./save/endpoint.txt", self.window)
+    sub_window = UrlSelectDialog(saved_items, "./save/endpoint.txt", self)
     if sub_window.exec() == QDialog.Accepted:
       endpoint = sub_window.get_selected_url()
       if endpoint:
-        self.window.findChild(QLineEdit, "lineEdit_2").setText(endpoint)
+        self.ui.lineEdit_2.setText(endpoint)
   def save_endpoint(self):
-    save_url(self.window.findChild(QLineEdit, "lineEdit_2").text(), "endpoint")
+    save_url(self.ui.lineEdit_2.text(), "endpoint")
   def save_url(self):
-    save_url(self.window.findChild(QLineEdit, "lineEdit").text(), "url")
+    save_url(self.ui.lineEdit.text(), "url")
+  def thread_request(self):
+     thread = threading.Thread(target=self.send_request, args=())
+     thread.start()
+
   def send_request(self):
-    url = self.window.findChild(QLineEdit, "lineEdit").text()
-    if not valid_prefix(url, self.logger):
+    url = self.ui.lineEdit.text()
+    if not valid_prefix(url, self.ui.textBrowser):
       return
-    endpoint = self.window.findChild(QLineEdit, "lineEdit_2").text()
-    type = self.window.findChild(QComboBox, "method_list").currentText()
-    check = self.window.findChild(QCheckBox, "checkBox").isChecked()
+    endpoint = self.ui.lineEdit_2.text()
+    type = self.ui.method_list.currentText()
+    check = self.ui.checkBox.isChecked()
     res = http_request(type, url+endpoint, self.request_body, check)
-    self.logger.append(f"请求已发送至{url+endpoint}")
-    self.logger.append(f"响应码: {res['status_code']}\n响应体: {res['body']}")
-  def clear_logger(self):
-    self.logger.clear()
+    self.ui.textBrowser.append(f"请求已发送至{url+endpoint}")
+    self.ui.textBrowser.append(f"响应码: {res['status_code']}\n响应体: {res['body']}")
   def listener(self):
-    self.window.findChild(QPushButton, "pushButton_4").clicked.connect(self.clear_logger)
-    self.window.findChild(QPushButton, "pushButton").clicked.connect(self.send_request)
-    self.window.findChild(QPushButton, "choose_1").clicked.connect(self.choose_url)
-    self.window.findChild(QPushButton, "choose_2").clicked.connect(self.choose_endpoint)
-    self.window.findChild(QPushButton, "save_1").clicked.connect(self.save_url)
-    self.window.findChild(QPushButton, "save_2s").clicked.connect(self.save_endpoint)
+    self.ui.pushButton_4.clicked.connect(self.clear_logger)
+    self.ui.pushButton.clicked.connect(self.thread_request)
+    self.ui.choose_1.clicked.connect(self.choose_url)
+    self.ui.choose_2.clicked.connect(self.choose_endpoint)
+    self.ui.save_1.clicked.connect(self.save_url)
+    self.ui.save_2s.clicked.connect(self.save_endpoint)
 if __name__ == "__main__":
   if not os.path.exists("./save"):
     os.makedirs("./save")
@@ -81,6 +89,6 @@ if __name__ == "__main__":
   with open("./save/url.txt", 'a', encoding='utf-8') as f:
         f.write('')
   app = QApplication(sys.argv)
-  main = Mainwindow()
-  main.window.show()
+  main = _MainWindow()
+  main.show()
   sys.exit(app.exec())
